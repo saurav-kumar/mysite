b90816843610d601d91cc08ed2ca22084aa7d2fc
saurav.kumar <saurav.kumar@edcellence.in>
2013-11-29 13:27:40 +0530

    Part 2 - Activated the admin site

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from polls.models import Question, Choice
 
-# Register your models here.
+# Register your models here.\
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text'] #if only to display without fieldsets
+    '''Adding fieldsets to categorize the fields''' 
+    fieldsets = [
+        ('Question' , {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    '''Replace with this if want to show/hide fields
+    ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    '''
+
+admin.site.register(Question, QuestionAdmin)
+
